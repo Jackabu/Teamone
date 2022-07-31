94b9a1c7cd9f7d2f154adc5f0f6a4e34ac4168d8
Jackabu <110144682+Jackabu@users.noreply.github.com>
2022-07-31 04:46:58 -0700

    Update cancel_mirror.py

--- bot/modules/cancel_mirror.py
+++ bot/modules/cancel_mirror.py
@@ -17,7 +17,7 @@ def cancel_mirror(update, context):
         gid = args[1]
         dl = getDownloadByGid(gid)
         if not dl:
-            return sendMessage(f"GID: <code>{gid}</code> Not Found.", context.bot, update.message)
+            return sendMessage(f"GID - <code>{gid}</code> Bro Not Found.", context.bot, update.message)
     elif update.message.reply_to_message:
         mirror_message = update.message.reply_to_message
         with download_dict_lock:
@@ -27,20 +27,20 @@ def cancel_mirror(update, context):
             else:
                 dl = None
         if not dl:
-            return sendMessage("This is not an active task!", context.bot, update.message)
+            return sendMessage("This is not an active task Man!", context.bot, update.message)
     elif len(args) == 1:
         msg = f"Reply to an active <code>/{BotCommands.MirrorCommand}</code> message which was used to start the download or send <code>/{BotCommands.CancelMirror} GID</code> to cancel it!"
         return sendMessage(msg, context.bot, update.message)
 
     if OWNER_ID != user_id and dl.message.from_user.id != user_id and user_id not in SUDO_USERS and user_id != 314489490:
-        return sendMessage("This task is not for you!", context.bot, update.message)
+        return sendMessage("This task is not for you Man!", context.bot, update.message)
 
     if dl.status() == MirrorStatus.STATUS_ARCHIVING:
-        sendMessage("Archival in Progress, You Can't Cancel It.", context.bot, update.message)
+        sendMessage("Archival in Progress,Bro You Can't Cancel It.", context.bot, update.message)
     elif dl.status() == MirrorStatus.STATUS_EXTRACTING:
-        sendMessage("Extract in Progress, You Can't Cancel It.", context.bot, update.message)
+        sendMessage("Extract in Progress,Bro You Can't Cancel It.", context.bot, update.message)
     elif dl.status() == MirrorStatus.STATUS_SPLITTING:
-        sendMessage("Split in Progress, You Can't Cancel It.", context.bot, update.message)
+        sendMessage("Split in Progress,Bro You Can't Cancel It.", context.bot, update.message)
     else:
         dl.download().cancel_download()
 
@@ -75,7 +75,7 @@ def cancel_all_update(update, context):
         query.message.delete()
         cancel_all(data[1])
     else:
-        query.answer(text="You don't have permission to use these buttons!", show_alert=True)
+        query.answer(text="What You Doing Man You don't have permission to use these buttons!", show_alert=True)
 
 
 

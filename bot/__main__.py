e0495f4a5ac476510bcc34a362ef138c4376a04f
Jackabu <110144682+Jackabu@users.noreply.github.com>
2022-08-12 15:13:32 -0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -131,7 +131,7 @@ help_string_telegraph = f'''<br>
 <br><br>
 <b>/{BotCommands.ZipWatchCommand}</b> [yt-dlp supported link]: Mirror yt-dlp supported link as zip
 <br><br>
-<b>/{BotCommands.LeechWatchCommand}</b> [yt-dlp supported link]: Leech yt-dlp supported link
+<b>/{BotCommands.LeechWatchCommand}</b> [Multiple Sites]: Zippyshare Instagram Youtube Gofiles supported links
 <br><br>
 <b>/{BotCommands.LeechZipWatchCommand}</b> [yt-dlp supported link]: Leech yt-dlp supported link as zip
 <br><br>
@@ -161,39 +161,12 @@ help_string_telegraph = f'''<br>
 <br><br>
 <b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the bot is hosted on
 '''
-
-
-sudo_help_string = f'''<br><br><b> Sudo/Owner Only Commands </b><br><br>
-<b>/{BotCommands.PingCommand}</b>: Check how long it takes to Ping the Bot
-<br><br>
-<b>/{BotCommands.AuthorizeCommand}</b>: Authorize a chat or a user to use the bot (Can only be invoked by Owner & Sudo of the bot)
-<br><br>
-<b>/{BotCommands.UnAuthorizeCommand}</b>: Unauthorize a chat or a user to use the bot (Can only be invoked by Owner & Sudo of the bot)
-<br><br>
-<b>/{BotCommands.AuthorizedUsersCommand}</b>: Show authorized users (Only Owner & Sudo)
-<br><br>
-<b>/{BotCommands.AddSudoCommand}</b>: Add sudo user (Only Owner)
-<br><br>
-<b>/{BotCommands.RmSudoCommand}</b>: Remove sudo users (Only Owner)
-<br><br>
-<b>/{BotCommands.RestartCommand}</b>: Restart and update the bot
-<br><br>
-<b>/{BotCommands.LogCommand}</b>: Get a log file of the bot. Handy for getting crash reports
-<br><br>
-<b>/{BotCommands.ShellCommand}</b>: Run commands in Shell (Only Owner)
-<br><br>
-<b>/{BotCommands.ExecHelpCommand}</b>: Get help for Executor module (Only Owner)
-<br><br>
-<b>/{BotCommands.AddleechlogCommand}</b>: Add Leech Log
-<br><br>
-<b>/{BotCommands.RmleechlogCommand}</b>: Remove Leech Log
-'''
 help_string = f'''
 Bro, Need Any Help!!
 '''
 help = telegraph.create_page(
         title='Dumbleech database',
-        content=help_string_telegraph + sudo_help_string,
+        content=help_string_telegraph,
     )["path"]
 
 def bot_help(update, context):
